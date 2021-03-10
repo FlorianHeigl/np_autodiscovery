e4fb79aa5cd05dce168a7f192fdd9ed5f5b17a4f
Florian Heigl <f.heigl@wartungsfenster.de>
2021-03-10 03:27:14 +0100

    Update __init__.py

--- np_autodiscovery/__init__.py
+++ np_autodiscovery/__init__.py
@@ -1,13 +1,13 @@
 default_app_config = 'np_autodiscovery.apps.NPAutoDiscoveryConfig'
 
 from extras.plugins import PluginConfig
-from .version import __version__
+#from .version import __version__
 
 class NpAutodiscoveryConfig(PluginConfig):
     name = 'np_autodiscovery'
     verbose_name = 'Autodiscovery'
     description = 'auto discovery of existing devices in a network'
-    version = '1.0.0'
+    version = 1.0.2
     author='John Anderson',
     author_email='lampwins@gmail.com',
     required_settings = []
